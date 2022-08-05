cc124b39d538caddb10e9c45085ca138785a329b
Sekaia <gillotte.bm@gmail.com>
2022-08-05 13:37:05 -0500

    using enumerate to loop through for loops

--- venv/python_sequences_practice.py
+++ venv/python_sequences_practice.py
@@ -2,11 +2,19 @@ groceries = ["eggs", "milk", "cereal", "turkey", "pudding", "pickles"]
 # iterating with basic for loops
 
 
-for item in groceries:
-    print(item)
+#for item in groceries:
+#    print(item)
 
 # iterating with enumerate
 
 # this is still a bit confusing for me, need to practice a bit more
 for index, item in enumerate(groceries, 1):
     print(f"{index}. {item}")
+
+word = input("Enter a word: \n")
+
+for count, letter in enumerate(word, 1):
+    print(letter)
+
+
+print(f"There are {count} letters in {word}")
