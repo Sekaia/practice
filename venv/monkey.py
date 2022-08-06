8fa5d904faa2c6d151de3789f49ba07bad8017e4
Sekaia <gillotte.bm@gmail.com>
2022-08-06 12:05:47 -0500

    added dunder string method and dunder iter, as well as added a jungle class

--- venv/monkey.py
+++ venv/monkey.py
@@ -13,12 +13,17 @@
 
 # class
 class Monkey:
-    def __init__(self, tail, arms, legs, type="Tamarin"):
+    def __init__(self, tail, arms, legs, age, type="Tamarin"):
         self.tail = tail
         self.arms = arms
         self.legs = legs
+        self.age = age
         self.type = type
         self.is_hungry = True
+
+    def __str__(self):
+        return f"{self.age} year old {self.type}"
+
     #method
     def speak(self):
         print("Ooo oo ah ah!")
@@ -43,9 +48,30 @@ class Monkey:
         else:
             print("Monkey not hungry.. yet..")
 
+class Jungle:
+    def __init__(self):
+        self.monkeys = []
+
+    def __iter__(self):
+        yield from self.monkeys
+
+    def add_monkey(self, monkey):
+        self.monkeys.append(monkey)
+
 #instantiating instances
-bobo = Monkey(True, 2, 2)
-taburi = Monkey(False, 0, 4, "Gorilla")
+bobo = Monkey(True, 2, 2, 3)
+taburi = Monkey(False, 0, 4, 18, "Gorilla")
+maburi = Monkey(True, 2, 2, 10, "Lion-tailed macaque")
+kaburi = Monkey(False, 0, 4, 28, "Black Howler")
+laburi = Monkey(True, 2, 2, 30, "Mandrill")
+monkey_jungle = Jungle()
+
+monkey_jungle.add_monkey(bobo)
+monkey_jungle.add_monkey(taburi)
+monkey_jungle.add_monkey(maburi)
+monkey_jungle.add_monkey(kaburi)
+monkey_jungle.add_monkey(laburi)
+
 
 bobo.speak()
 taburi.steal("banana")
@@ -54,6 +80,12 @@ taburi.climb(10)
 bobo.eat("banana")
 taburi.eat("human")
 bobo.eat("taburi")
+
+print(bobo)
+print(taburi)
+
+for monkey in monkey_jungle:
+    print(monkey)
 
 
 
